bfc25d1b94f79095153be24e031b0531bb7d6cec
Theofilos Papadopoulos <priestont@gmail.com>
2022-07-09 16:31:06 +0300

    publish gh page

---
+++ build/precache-manifest.cb8ba4ef5c660c99ebec9f0f634fae44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12fc3ab98b1037d24044c3182d1f8db",
+    "url": "/IoT-MR/index.html"
+  },
+  {
+    "revision": "20813f2c1d26ee3c1951",
+    "url": "/IoT-MR/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "0c2c90e7d43c82cc6ffa",
+    "url": "/IoT-MR/static/css/main.cdb08601.chunk.css"
+  },
+  {
+    "revision": "20813f2c1d26ee3c1951",
+    "url": "/IoT-MR/static/js/2.35c0b05e.chunk.js"
+  },
+  {
+    "revision": "e747d0f2309ecd9158967988ee28ce4e",
+    "url": "/IoT-MR/static/js/2.35c0b05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2c90e7d43c82cc6ffa",
+    "url": "/IoT-MR/static/js/main.ee127649.chunk.js"
+  },
+  {
+    "revision": "c0270e7ed31e5ba283f7",
+    "url": "/IoT-MR/static/js/runtime-main.b264b874.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e48f30400def33afe742249cbd0f2f67.js"
+  "/IoT-MR/precache-manifest.cb8ba4ef5c660c99ebec9f0f634fae44.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IoT-MR/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
